47e1e4fefe0ac424d3a832fb7ddcce87bac13b81
jstrumwasser <38440681+jstrumwasser@users.noreply.github.com>
2018-04-16 16:18:08 -0400

    Update runme.ps1

--- runme.ps1
+++ runme.ps1
@@ -1,8 +1,20 @@
-//rename qfiles.csv as date qfilesdate.csv//
+#rename qfiles.csv as date qfilesdate.csv#
 gam redirect csv .\qfiles.csv user quarrantine@challengecharterschools.org print filelist id name permissions
 
-//rename deleteqfiles.csv as date deleteqfilesdate.csv//
+#Debug
+#Write-Host "Press any key to continue ..."
+#$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+#rename deleteqfiles.csv as date deleteqfilesdate.csv
 python GetUserNonOwnerDrivePermissions.py .\qfiles.csv .\deleteqfiles.csv
 
-//rename deleteqfiles.csv as date deleteqfilesdate.csv//
+#Debug
+#Write-Host "Press any key to continue ..."
+#$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+#rename deleteqfiles.csv as date deleteqfilesdate.csv
 gam csvkmd users .\deleteqfiles.csv keyfield Owner subkeyfield driveFileId datafield permissionIds delimiter "," delete permissions csvsubkey driveFileId csvdata permissionIds
+
+#Debug
+#Write-Host "Press any key to continue ..."
+#$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
